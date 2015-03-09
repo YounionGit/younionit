afe7cd2e4bd5ee571e3a1a2f175ebe3252fbebf0
YounionGit <cassiano.trevisan@younionit.com>
2015-03-09 16:28:44 -0300

    usuarios

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@
 var app = angular.module('younionApp', [
   'ngRoute',
   'ControlerApp',
+  'UsuariosApp',
   'ngCookies'
 ]);
 
--- modules/admin/usuarios/usuarios.js
+++ modules/admin/usuarios/usuarios.js
@@ -4,6 +4,24 @@ var usuariosApp = angular.module('UsuariosApp',[]);
 
 usuariosApp.controller('UsuariosCtrl', function($rootScope, $scope, $http, $location) {
 	
+	loadGrid();
 	
+	$scope.save = function(){
+		
+		var login = $scope.login;
+		var nome = $scope.nome;
+		var senha = $scope.password;
+				
+		loadGrid();
+		
+	};
+	
+	function loadGrid(){
+		$http.post('/usuarios/list')
+	    .success(function (res) {
+	    	console.log(res);    	
+	    	$scope.usuarios = res;
+	    });
+	};
 	
 });
