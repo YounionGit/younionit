d9b3f989b3704f9bfe3dbcd44f129730cf010ee8
YounionGit <cassiano.trevisan@younionit.com>
2015-03-10 09:21:56 -0300

    melhorias, liberacao mes controle horas

--- js/app.js
+++ js/app.js
@@ -35,6 +35,7 @@ app.config(['$routeProvider', function ($routeProvider) {
      .when("/politicas", {templateUrl: "modules/politicas/politicas.html", controller: "PageCtrl"})
      //ADMIN
       .when("/usuarios/cadastrar", {templateUrl: "modules/admin/usuarios/usuarios.html", controller: "PageCtrl"})
+      .when("/controles/liberacao/edicao", {templateUrl: "modules/controles/liberacaoControleMes.html", controller: "PageCtrl"})
       // else 404
     .otherwise("/404", {templateUrl: "modules/404.html", controller: "PageCtrl"});
 }]);
