bbbf1b29cc38527053d76a5ed387e6cc21a938a0
Bruno Varoni Palma <brunopalma@gmail.com>
2015-04-18 14:51:28 -0300

    Refactoring code style

--- modules/admin/usuarios/usuarios.js
+++ modules/admin/usuarios/usuarios.js
@@ -1,151 +1,204 @@
-var usuariosApp = angular.module('UsuariosApp', []);
-usuariosApp.controller('UsuariosCtrl', function ($rootScope, $scope, $http, $location, $modal, $document, $timeout) {
-  loadGrid();
-  $scope.open = function (size) {
-    var modalInstance = $modal.open({
-      templateUrl : 'modules/admin/usuarios/usuarioEditar.html',
-      controller  : 'ModalUsuariosEditarCtrl',
-      size        : size,
-      resolve     : {
-        usuario : function () {
-          return {ativo : "1", id_perfil : 2};
-        }
-      }
-    });
-    modalInstance.result.then(function (res) {
-      loadGrid();
-      showSuccess("Usuário criado com sucesso.");
-    });
-  };
-  $scope.editar = function (entity) {
-    var modalInstance = $modal.open({
-      templateUrl : 'modules/admin/usuarios/usuarioEditar.html',
-      controller  : 'ModalUsuariosEditarCtrl',
-      resolve     : {
-        usuario : function () {
-          return entity;
-        }
-      }
-    });
-    modalInstance.result.then(function (res) {
-      loadGrid();
-      showSuccess("Usuário atualizado com sucesso.");
-    });
-  };
-  $scope.mostrar = function (entity) {
-    var modalInstance = $modal.open({
-      templateUrl : 'modules/admin/usuarios/usuarioMostrar.html',
-      controller  : 'ModalUsuariosMostrarCtrl',
-      resolve     : {
-        usuario : function () {
-          return entity;
-        }
-      }
-    });
-  };
-  $scope.apagar = function (entity) {
-    if (entity.ativo == 0) {
-      showError("Usuário já está inativo.")
-    } else {
-      var r = confirm("Deseja desativar o usuário? ", "");
-      if (r == true) {
-        $http.post('/usuarios/apagar', { entity : entity}).success(function (res) {
-          showSuccess("Usuário desativado com sucesso.");
-          loadGrid();
-        });
-      }
-    }
-  };
-  function loadGrid() {
-    $http.post('/usuarios/list').success(function (res) {
-      $scope.usuarios = res;
-    });
-  }
 
-  function hideMsg() {
-    $scope.showMsg = false;
-  }
 
-  function showSuccess(msg) {
-    $scope.showMsg = true;
-    $scope.msgController = msg;
-    $scope.classMsgController = "alert alert-success";
-    $timeout(hideMsg, 3000);
-  }
+var usuariosApp = angular.module('UsuariosApp',[]);
 
-  function showError(msg) {
-    $scope.showMsg = true;
-    $scope.msgController = msg;
-    $scope.classMsgController = "alert alert-danger";
-    $timeout(hideMsg, 3000);
-  }
+
+usuariosApp.controller('UsuariosCtrl', function($rootScope, $scope, $http, $location, $modal, $document, $timeout) {
+	
+	loadGrid();
+	
+	$scope.open = function (size) {
+		
+		 var modalInstance = $modal.open({
+		      templateUrl: 'modules/admin/usuarios/usuarioEditar.html',
+		      controller: 'ModalUsuariosEditarCtrl',
+		      size: size,
+		      resolve: {
+		        usuario: function () {
+		          return {ativo:"1", id_perfil:2};
+		        }
+		      }		 
+		    });
+		 
+		 modalInstance.result.then(function (res) {			
+			 loadGrid();
+	        showSuccess("Usuário criado com sucesso.");
+		    });		 
+	};
+	
+	
+	$scope.editar = function(entity){		
+		var modalInstance = $modal.open({
+		      templateUrl: 'modules/admin/usuarios/usuarioEditar.html',
+		      controller: 'ModalUsuariosEditarCtrl',		     
+		      resolve: {
+			        usuario: function () {
+			          return entity;
+			        }
+			      }
+		    });	
+		
+		modalInstance.result.then(function (res) {			 
+			 loadGrid();
+	         showSuccess("Usuário atualizado com sucesso.");
+	    });
+		
+	};
+	
+	
+	
+	$scope.mostrar = function(entity){		
+		var modalInstance = $modal.open({
+		      templateUrl: 'modules/admin/usuarios/usuarioMostrar.html',
+		      controller: 'ModalUsuariosMostrarCtrl',		      
+		      resolve: {
+			        usuario: function () {
+			          return entity;
+			        }
+			      }
+		    });	
+		
+	};
+	
+	
+	$scope.apagar = function(entity){
+		if(entity.ativo == 0){
+			showError("Usuário já está inativo.")
+		}else{
+			var r = confirm("Deseja desativar o usuário? ","");
+			if(r == true){
+				$http.post('/usuarios/apagar', { entity: entity})
+		        .success(function (res) {
+		        	showSuccess("Usuário desativado com sucesso.");
+		        	loadGrid();
+		        });
+			}
+		}
+	};
+
+	
+	function loadGrid(){		
+		$http.post('/usuarios/list')
+	    .success(function (res) {	    	
+	    	$scope.usuarios = res;
+	    });				
+	}
+	
+	 function hideMsg() {
+         $scope.showMsg = false;
+     }
+	 
+	 function showSuccess(msg){
+		$scope.showMsg = true;
+		$scope.msgController = msg;
+     	$scope.classMsgController = "alert alert-success";
+     	$timeout(hideMsg, 3000);
+	 }
+	 
+	 function showError(msg){
+		$scope.showMsg = true;
+		$scope.msgController = msg;
+     	$scope.classMsgController = "alert alert-danger";
+     	$timeout(hideMsg, 3000);
+	 }
+	
 });
-usuariosApp.controller('ModalUsuariosMostrarCtrl', function ($scope, $http, $modalInstance, $filter, usuario) {
-  $scope.loadDadosPessoais = function () {
-    $http.post('/usuarios/dados/list', {usuario : usuario}).success(function (res) {
-      $scope.usuario = res;
-    });
-  };
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-  $scope.editar = function () {
-    $scope.editavel = !$scope.editavel;
-    $scope.loadTipoContratacao();
-  };
-  $scope.salvar = function () {
-    var usuario = $scope.usuario;
-    $http.post('/usuarios/dados/salvar', {usuario : usuario}).success(function (res) {
-      $modalInstance.close(res);
-    });
-  };
-  $scope.loadTipoContratacao = function () {
-    $http.post('/usuarios/dados/contratacao/list').success(function (res) {
-      $scope.tipoContratacao = res;
-    });
-  };
-  $scope.loadDadosPessoais();
+
+usuariosApp.controller('ModalUsuariosMostrarCtrl', function ($scope,$http, $modalInstance, $filter, usuario) {
+	
+	$scope.loadDadosPessoais = function(){
+		$http.post('/usuarios/dados/list', {usuario: usuario})
+	    .success(function (res) {
+	    	
+	    	$scope.usuario = res;
+	    });		
+	};
+	
+	
+	$scope.cancel = function () {
+	    $modalInstance.dismiss('cancel');
+	};
+	
+	
+	$scope.editar = function(){
+		$scope.editavel = ! $scope.editavel;
+		$scope.loadTipoContratacao();
+	};
+	
+	$scope.salvar = function(){		
+		var usuario = $scope.usuario;
+
+		$http.post('/usuarios/dados/salvar', {usuario: usuario})
+	    .success(function (res) {
+	    	$modalInstance.close(res);
+	    });
+	};
+	
+	$scope.loadTipoContratacao = function(){
+		$http.post('/usuarios/dados/contratacao/list')
+	    .success(function (res) {
+	    	$scope.tipoContratacao = res;		    	
+	    });
+		
+	};
+		
+	$scope.loadDadosPessoais();
+	
 });
-usuariosApp.controller('ModalUsuariosEditarCtrl', function ($scope, $http, md5, $modalInstance, usuario) {
-  $scope.save = function () {
-    var usuario = $scope.usuario;
-    if (usuario.senha !== usuario.senha2) {
-      $scope.showMsg = "Senha deve ser igual.";
-      $scope.classMsg = "alert alert-danger";
-    } else
-      if (usuario.id_usuario === undefined && (usuario.senha2 === undefined || usuario.senha === undefined)) {
-        $scope.showMsg = "Informe uma senha para o usuário.";
-        $scope.classMsg = "alert alert-danger";
-      } else {
-        if (usuario.senha !== undefined) {
-          usuario.senhaMD5 = md5.createHash(usuario.senha);
-          usuario.senha = "";
-          usuario.senha2 = "";
-        }
-        $http.post('/usuarios/salvar', {usuario : usuario}).success(function (res) {
-          console.log(res);
-          if (res.code === "ER_DUP_ENTRY") {
-            $scope.showMsg = "Este Login já existe";
-            $scope.classMsg = "alert alert-danger";
-          } else {
-            $modalInstance.close(res);
-          }
-        });
-      }
-  };
-  $scope.loadPerfil = function () {
-    $http.post('/usuarios/perfil/list').success(function (res) {
-      $scope.perfis = res;
-    });
-  };
-  $scope.loadItems = function (usuario) {
-    if (usuario !== undefined) {
-      $scope.usuario = usuario;
-    }
-  }
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-  $scope.loadPerfil();
-  $scope.loadItems(usuario);
+
+usuariosApp.controller('ModalUsuariosEditarCtrl', function ($scope,$http,md5, $modalInstance, usuario) {
+			
+	$scope.save = function(){
+		var usuario = $scope.usuario;
+		
+		if(usuario.senha !== usuario.senha2){
+			$scope.showMsg = "Senha deve ser igual.";
+			$scope.classMsg = "alert alert-danger";
+			
+		}else if(usuario.id_usuario ===undefined && (usuario.senha2 === undefined || usuario.senha === undefined) ){
+			$scope.showMsg = "Informe uma senha para o usuário.";
+			$scope.classMsg = "alert alert-danger";			
+		}else{
+			if(usuario.senha !== undefined){
+				usuario.senhaMD5 = md5.createHash(usuario.senha);
+				usuario.senha = "";
+				usuario.senha2 = "";
+			}
+			$http.post('/usuarios/salvar', {usuario: usuario})
+		    .success(function (res) {		
+		    	console.log(res);
+		    	if(res.code === "ER_DUP_ENTRY"){
+		    		$scope.showMsg = "Este Login já existe";
+					$scope.classMsg = "alert alert-danger";
+		    	}else{
+		    		$modalInstance.close(res);
+		    	}
+		    });
+		}
+	};
+	
+	
+	
+	$scope.loadPerfil = function(){
+		$http.post('/usuarios/perfil/list')
+	    .success(function (res) {	    	
+	    	$scope.perfis = res;
+	    });
+		
+	};
+	
+	$scope.loadItems = function(usuario){
+
+		if(usuario !== undefined){
+			$scope.usuario = usuario;
+		}
+	}
+	
+	$scope.cancel = function () {
+	    $modalInstance.dismiss('cancel');
+	};
+	  
+	$scope.loadPerfil();
+	$scope.loadItems(usuario);
+	
 });
--- modules/controles/controles.js
+++ modules/controles/controles.js
@@ -1,95 +1,134 @@
 // main.js
 var controlerApp = angular.module('ControlerApp', ['ngGrid']);
-controlerApp.controller('ControlerCtrl', function ($rootScope, $scope, $http, $location, $timeout) {
-  var user = $rootScope.globals.currentUser;
-  $scope.gridOptions = {
-    data                : 'myData',
-    enableRowSelection  : false,
-    enableCellSelection : true,
-    plugins             : [new ngGridCsvExportPlugin()],
-    showFooter          : true,
-    columnDefs          : [
-      {field         : 'data', displayName : 'Dia do Mês',
-        cellTemplate : 'modules/controles/datepicker.html'},
-      {field         : 'hora_entrada', displayName : 'Entrada',
-        cellTemplate : 'modules/controles/timepicker_entrada.html'},
-      {field         : 'hora_saida', displayName : 'Saída',
-        cellTemplate : 'modules/controles/timepicker_saida.html'},
-      {field : 'total_Horas', displayName : 'Total Horas', enableCellEdit : false},
-      {field : 'atividade', displayName : 'Atividade', enableCellEdit : true},
-      {field : 'observacao', displayName : 'Observação', enableCellEdit : true},
-      {cellTemplate : 'modules/controles/icones.html', enableRowSelection : false}
-    ]
-  };
-  $scope.add = function () {
-    var id = user.id;
-    var newRow = [
-      {id_usuario : id}
-    ];
-    $scope.myData = $scope.myData.concat(newRow);
-  };
-  $scope.salvar = function (entity) {
-    if (validaTabela(entity, $scope)) {
-      $http.post('/horarios/salvar', { entity : entity}).success(function (res) {
-        $scope.msgController = "Horário salvo com sucesso.";
-        $scope.classMsgController = "alert alert-success";
-      });
-    } else {
-      $scope.classMsgController = "alert alert-danger";
-    }
-    $timeout(hideMsg, 5000);
-    $scope.loadGrid();
-  };
-  function hideMsg() {
-    $scope.msgController = false;
-  }
 
-  $scope.apagar = function (entity, rowid) {
-    if (entity.id === undefined) {
-      $scope.myData.splice(rowid, 1);
-    } else {
-      var r = confirm("Deseja apagar o registro? \n\n Atividade:  " + entity.atividade);
-      if (r == true) {
-        $http.post('/horarios/apagar', { entity : entity}).success(function (res) {
-          $scope.msgController = "Horario removido com sucesso.";
-          $scope.classMsgController = "alert alert-success";
-        });
-        $scope.myData.splice(rowid, 1);
-      }
-      $timeout(hideMsg, 5000);
+controlerApp.controller('ControlerCtrl', function($rootScope, $scope, $http, $location, $timeout) {
+
+	var user = $rootScope.globals.currentUser;   
+        
+    $scope.gridOptions = {
+        data: 'myData',
+        enableRowSelection: false,
+        enableCellSelection: true,
+        plugins: [new ngGridCsvExportPlugin()],
+        showFooter: true,
+        columnDefs: [
+            {field:'data', displayName:'Dia do Mês', 
+            	cellTemplate: 'modules/controles/datepicker.html'},
+            {field:'hora_entrada', displayName:'Entrada',
+            		cellTemplate: 'modules/controles/timepicker_entrada.html'},
+            {field:'hora_saida', displayName:'Saída',
+            		cellTemplate: 'modules/controles/timepicker_saida.html'},
+            {field:'total_Horas', displayName:'Total Horas', enableCellEdit: false},
+            {field:'atividade', displayName:'Atividade', enableCellEdit: true},
+            {field:'observacao', displayName:'Observação',enableCellEdit: true},
+            {cellTemplate: 'modules/controles/icones.html', enableRowSelection: false}]
+    };
+    
+    $scope.add = function (){
+    	
+    	var id = user.id;
+    	
+        var newRow = [{id_usuario: id}];
+        
+        $scope.myData = $scope.myData.concat(newRow);
+        
+    };
+    
+    $scope.salvar = function (entity){
+    	
+    	if(validaTabela(entity, $scope)){
+    		$http.post('/horarios/salvar', { entity: entity})
+            .success(function (res) {
+            	$scope.msgController = "Horário salvo com sucesso.";
+            	$scope.classMsgController = "alert alert-success";
+            });
+    	}else{
+    		$scope.classMsgController = "alert alert-danger";
+    	}
+    	$timeout(hideMsg, 5000);
+    	$scope.loadGrid();
+    };
+    
+    function hideMsg() {
+        $scope.msgController = false;
     }
-  };
-  $scope.changeDate = function () {
-    var year = $scope.selectedYear;
-    var month = $scope.selectedMonth;
-    var user = $rootScope.globals.currentUser;
-    $http.post("/horarios/fechamento/mes", {user : user, month : month, year : year}).success(function (response) {
-      $scope.editavel = response.flag;
-    })
-    $scope.loadGrid();
-  };
-  $scope.loadGrid = function () {
-    var year = $scope.selectedYear;
-    var month = $scope.selectedMonth;
-    var user = $rootScope.globals.currentUser;
-    $scope.myData = null;
-    $http.post("/horarios/list", {user : user, month : month, year : year}).success(function (response) {
-      $scope.myData = response;
-    })
-  }
-  function validaTabela(entity, $scope) {
-    var resposta = true;
-    if (entity.data === undefined || entity.hora_entrada === undefined || entity.hora_saida === undefined || entity.atividade === undefined || entity.observacao === undefined) {
-      $scope.msgController = 'Favor preencher todos os campos.';
-      resposta = false;
+    
+    $scope.apagar = function (entity, rowid){
+    	if(entity.id === undefined){
+    		$scope.myData.splice(rowid,1);
+    	}else{
+    		var r = confirm("Deseja apagar o registro? \n\n Atividade:  "+entity.atividade);
+    		
+    		if (r == true) {
+    			$http.post('/horarios/apagar', { entity: entity})
+    			.success(function (res) {
+    				$scope.msgController = "Horario removido com sucesso.";
+    				$scope.classMsgController = "alert alert-success";
+    			});
+    			$scope.myData.splice(rowid,1);
+    		}
+    		$timeout(hideMsg, 5000);    		
+    	}
+    	    	
+    };
+    
+   
+    $scope.changeDate = function (){
+    	var year = $scope.selectedYear;
+    	var month = $scope.selectedMonth;
+    	var user = $rootScope.globals.currentUser;
+    	
+    	
+    	 $http.post("/horarios/fechamento/mes", {user: user, month: month, year: year})
+         .success(function(response) {
+         	$scope.editavel = response.flag;
+        })   	
+    	
+        $scope.loadGrid();
+    };   
+    
+    $scope.loadGrid = function(){
+    	var year = $scope.selectedYear;
+    	var month = $scope.selectedMonth;
+    	
+    	var user = $rootScope.globals.currentUser;
+    	
+    	$scope.myData = null;
+    	
+    	$http.post("/horarios/list", {user: user, month: month, year: year})
+         .success(function(response) {
+         	$scope.myData = response;
+        })   
+              
     }
-    if (entity.hora_entrada >= entity.hora_saida) {
-      $scope.msgController = "A Hora de saida deve ser maior que a entrada.";
-      resposta = false;
-    }
-    return resposta;
-  };
-  $scope.selectedYear = new Date().getFullYear();
-  $scope.selectedMonth = new Date().getMonth() + 1;
-  $scope.changeDate();
+    
+    function validaTabela (entity, $scope){
+    	var resposta = true;
+    		
+    	if(entity.data === undefined ||
+    			entity.hora_entrada === undefined ||
+    			entity.hora_saida === undefined ||
+    			entity.atividade === undefined ||
+    			entity.observacao === undefined){
+    		
+    		 $scope.msgController = 'Favor preencher todos os campos.';
+    		 resposta = false;
+    	}
+    	
+    	if(entity.hora_entrada >= entity.hora_saida ){
+    		$scope.msgController = "A Hora de saida deve ser maior que a entrada.";
+    		resposta = false;
+    	}
+    	
+    	return resposta;
+    };
+    
+    
+    $scope.selectedYear = new Date().getFullYear();
+    
+	$scope.selectedMonth = new Date().getMonth() + 1;
+	
+	$scope.changeDate();
+	
+    
 });
